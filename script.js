3a2abe82ebe802d9bc12848a36dd3929978820e1
indre101 <zygaityte.indre@gmail.com>
2020-03-09 16:17:47 +0100

    prepend

--- script.js
+++ script.js
@@ -3,27 +3,41 @@ const svgTimelineContainer = document.querySelector(".svgTimelineContainer");
 const svgContainer = document.querySelector("#svgContainer");
 
 
+window.addEventListener("DOMContentLoaded", init)
+
+function init() {
+  getTheSVG();
+
+}
+
 async function getTheSVG() {
+
+  // Append timeline  to a div
+  const responseTimeline = await fetch("./timeline.svg");
+  const svgTimeline = await responseTimeline.text();
+  svgTimelineContainer.innerHTML = svgTimeline;
+
+  // Append info box to a div
   const responseInfobox = await fetch("./infobox.svg");
   const svgInfobox = await responseInfobox.text();
   svgInfoBoxContainer.innerHTML = svgInfobox;
 
-  const responseTimeline = await fetch("./timeline.svg");
-  const svgTimeline = await responseTimeline.text();
-  svgTimelineContainer.innerHTML = svgTimeline;
   appendSVG()
 }
 
-getTheSVG()
 
 
 function appendSVG() {
-  const timeleline = document.createElementNS("http://www.w3.org/2000/svg", "use");
+
+  // Append info box inside the timeline
   const infobox = document.createElementNS("http://www.w3.org/2000/svg", "use");
+  infobox.href.baseVal = "#infoBox";
+  document.querySelector("#timeline").appendChild(infobox);
 
+  // Append timeleline into the svg container
+  const timeleline = document.createElementNS("http://www.w3.org/2000/svg", "use");
   timeleline.href.baseVal = "#timeline"
-  infobox.href.baseVal = "#infoBox"
-
   svgContainer.appendChild(timeleline);
-  svgContainer.appendChild(infobox);
+
+
 }
